ba376d9bb363103f38f3d902baee869555248832
redripper480 <jzoraz@gmail.com>
2022-10-21 02:25:32 +0500

    ACE and File Input

--- src/app.js
+++ src/app.js
@@ -42,7 +42,26 @@ app.post('/ace', function (request, response, next){
     response.sendFile(indexPath);
 });
 
-app.post('')
+app.post('/file', function(request, response){
+    var form = new formidable.IncomingForm({ hashAlgorithm: 'sha256' });
+    form.parse(request)
+    form.on('file', (name, file) => {
+        const ext = path.extname(file.originalFilename)
+        const new_path = path.join(__dirname, '../public/uploads/' + file.hash)
+        fs.rename(file.filepath, new_path + ext, (err) => { if (err) console.error(`ERROR: ${err}`) });
+        file.filepath = new_path
+        console.log('Uploaded ' + file.hash)
+        if (ext === '.cpp') {
+            exec(`g++ -O2 -std=c++17 "${file.filepath}.cpp" -o "${file.filepath}"`, (err, stdout, stderr) => {
+                if (err) console.error(`ERROR: ${err}`)
+                else if (stderr) console.log(`stderr: ${stderr}`)
+                else if (stdout) console.log(`stdout: ${stdout}`)
+                exec(`"${file.filepath}"`, (stdout) => { if (stdout) console.log(stdout) });
+            })   
+        }
+    });
+    response.sendFile(indexPath);
+})
 
 const PORT = 5000
 app.listen(PORT, () => {
