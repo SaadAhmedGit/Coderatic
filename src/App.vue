0a98414e682c1654747943b55ca2c498d8fcf69d
SaadAhmedGit <saadahmedcronos@gmail.com>
2022-10-25 21:15:43 +0500

    Added ace editor

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,39 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <v-ace-editor
+    id="editor"
+    v-model:value="content"
+    @init="editorInit"
+    lang="c_cpp"
+    theme="dracula"
+    :options="{
+      autoScrollEditorIntoView: true,
+      copyWithEmptySelection: true,
+      enableBasicAutocompletion: true,
+      enableSnippets: true,
+      enableLiveAutocompletion: true,
+    }"
+  />
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+<script lang="ts">
+//import CodeEditor from "./components/CodeEditor.vue";
+import { VAceEditor } from "vue3-ace-editor";
+export default {
+  name: "App",
+  components: {
+    VAceEditor,
+  },
+};
+</script>
+<style scoped>
+#editor {
+  height: 600px;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  font-size: large;
+  left: 0;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  border-bottom-right-radius: 0px;
 }
 </style>
