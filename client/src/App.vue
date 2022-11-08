f88ef4e68caca23675a3c712c9e2efdc3db81d68
SaadAhmedGit <saadahmedcronos@gmail.com>
2022-11-08 22:59:38 +0500

    Added ts-node-dev

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,31 +1,31 @@
 <template>
-<div class="container-fluid">
-  <SubmissionPage @submitCode="recieved"></SubmissionPage>
-</div>
+  <div class="container-fluid">
+    <SubmissionPage @submitCode="recieved"></SubmissionPage>
+  </div>
 </template>
 
-<script>
+<script lang="ts">
 import SubmissionPage from "./components/SubmissionPage.vue";
 export default {
   name: "App",
   components: {
     SubmissionPage,
   },
-  methods:{
-    recieved(code){
-      console.log(code)
-    }
+  methods: {
+    recieved(code: string) {
+      console.log(code);
+    },
   },
 };
 </script>
 
 <style>
-#centerContained{
+#centerContained {
   text-align: center;
   padding: 1% 0;
 }
-.container-fluid{
-    padding: 3% 15%;
-    height: 90%;
+.container-fluid {
+  padding: 3% 15%;
+  height: 90%;
 }
 </style>
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,18 @@
-import { Request, Response, Application, express } from "express";
-//const express = require("express");
-import * as bodyParser from "body-parser";
+import express from 'express';
+import cors from "cors";
 
-const app = express();
-app.use(bodyParser.json());
+const app: express.Application = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
 
-app.get("/status", (req: Request, res: Response) => {
+type Request = express.Request;
+type Response = express.Response;
+
+app.get("/server", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-const PORT = 8081;
-app.listen(PORT, () => {
-  console.log(`Listening on PORT: ${PORT}...`);
+app.listen(process.env.PORT || 8081, () => {
+  console.log("Server is running on port 8081");
 });
